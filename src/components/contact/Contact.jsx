ee17c824ab8cdc4c323ba6ea2f436b1fadfd22cc
Lucas Pedroso <lucas.scpedroso@gmail.com>
2023-07-24 19:10:42 +0100

    created and styled Contact component, and installed emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import { useRef } from 'react'
 import './contact.css'
+import emailjs from 'emailjs-com'
+
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_wf9mi6j', 'template_8d8ztux', form.current,'brs63geBpdy1zgh8h')
+
+    e.target.reset()
+  }
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className='contact_option'>
+           <MdOutlineEmail className='contact_option-icon'/>
+            <h4>Email</h4>
+            <h5>lucas.scpedroso@gmail.com</h5>
+            <a href="mailto:lucas.scpedroso@gmail.com" target='_blank'>Send a message</a>
+          </article>
+
+          <article className='contact_option'>
+           <BsWhatsapp className='contact_option-icon'/>
+            <h4>WhatsApp</h4>
+            <a href="https://api.whatsapp.com/send?phone+4407484165082" target='_blank'>Send a message</a>
+          </article>
+        </div>
+
+        {/* END OF CONTACT OPTIONS */}
+
+        <form ref={form} onSubmit={sendEmail} >
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,73 @@
 import React from 'react'
 import './portfolio.css'
+import portImage1 from '../../assets/portfolio-img.png'
+import portImage2 from '../../assets/portfolio-img.png'
+import portImage3 from '../../assets/portfolio-img.png'
+import portImage4 from '../../assets/portfolio-img.png'
+
+
+const data = [
+  {
+    id: 1,
+    image: portImage1,
+    title: 'Insurance company website',
+    description: 'An website for an insurance company which was built mainly using React for the frontend and NodeJS for the backend',
+    github: 'https://github.com/lucascpedroso',
+    demo: 'https://www.cardtotalseguros.com.br/'
+  },
+  {
+    id: 2,
+    image: portImage2,
+    title: 'Insurance company website',
+    description: 'An website for an insurance company which was built mainly using React for the frontend and NodeJS for the backend',
+    github: 'https://github.com/lucascpedroso',
+    demo: 'https://www.cardtotalseguros.com.br/'
+  },
+  {
+    id: 3,
+    image: portImage3,
+    description: 'An website for an insurance company which was built mainly using React for the frontend and NodeJS for the backend',
+    title: 'Insurance company website',
+    github: 'https://github.com/lucascpedroso',
+    demo: 'https://www.cardtotalseguros.com.br/'
+  },
+  {
+    id: 4,
+    image: portImage4,
+    description: 'An website for an insurance company which was built mainly using React for the frontend and NodeJS for the backend',
+    title: 'Insurance company website',
+    github: 'https://github.com/lucascpedroso',
+    demo: 'https://www.cardtotalseguros.com.br/'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        {
+          data.map(({ id, image, title, description, github, demo }) => {
+            return (
+              <article key={id} className='portfolio_item'>
+                <div className="portfolio_item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <div className="portfolio_item-cta">
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+      
+    </section>
   )
 }
 
