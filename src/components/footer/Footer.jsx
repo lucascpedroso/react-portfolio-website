45e2d217e55429a832d2f7ef77684a2e9b619d0e
Lucas Pedroso <lucas.scpedroso@gmail.com>
2023-07-26 18:35:58 +0100

    created and styled the footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
 
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://www.linkedin.com/in/lucas-pedroso-c/"><BsLinkedin /></a>
+        <a href="github.com/lucascpedroso"><FaGithub /></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; Lucas Pedroso. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
